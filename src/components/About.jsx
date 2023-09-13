0376842125d13fc27b6eadf917aa20c987867fbb
Devsherile <cheptoosherile@gmail.com>
2023-09-13 23:25:09 +0300

    portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,24 @@
+import './About.css'
+
+
 function About() {
     return (
-        <>
-            <p>
-                I'm a Front End Engineer and Software Engineer with a focus on creating responsive and highly interactive web/mobile applications.
-                I am well versed in best practices for front end development using JavaScript (React, Vanilla) and Mobile Development (React Native) and User Interaction.
-                I have built frontend projects that aim at changing the world.
-            </p>
-            <h2>What can limit me from other learners?</h2>
-            <button>Check out my projects</button>
-            <img src="./image/setup.jpg" alt="About" />
-        </>
+        <div className="About">
+
+            <div className='container'>
+                <h1>About</h1>
+                <p>
+                    I'm a Front End Engineer and Software Engineer with a focus on creating responsive and highly interactive web/mobile applications.
+                    I am well versed in best practices for front end development using JavaScript (React, Vanilla) and Mobile Development (Flutter) and User Interaction.
+                    I have built frontend projects that aim at changing the world.
+                </p>
+                <h2>What can limit me from other learners?</h2>
+                <button>Check out my projects</button>
+            </div>
+            <div className='img'>
+                <img src="./image/setup.jpg" alt="About" />
+            </div>
+        </div>
     )
 }
 
